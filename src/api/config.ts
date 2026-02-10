0b23397e430443fc76c912a30fa9d0771b4de25d
Idan malka <Idanmal8@gmail.com>
2026-02-10 21:56:42 +0200

    feat: configure production API_BASE_URL when in production environment.

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,1 +1,3 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '/api'
+export const API_BASE_URL = import.meta.env.PROD
+  ? 'https://animake-api-997459958439.europe-west1.run.app'
+  : import.meta.env.VITE_API_BASE_URL || '/api'
